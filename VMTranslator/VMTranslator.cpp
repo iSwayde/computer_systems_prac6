64129e93e1fdc89df998fae88d7d0386dae33765
unknown <danielmimportant@hotmail.com>
2023-10-20 22:47:59 +1030

    adjusted push code to increment stack pointer at the end

--- VMTranslator/VMTranslator.cpp
+++ VMTranslator/VMTranslator.cpp
@@ -24,36 +24,36 @@ string VMTranslator::vm_push(string segment, int offset){
     temp += to_string(offset);
     //constant
     if (segment == "constant"){
-        temp += "\nD=A\n@SP\nAM=M+1\nA=A-1\nM=D\n";
+        temp += "\nD=A\n@SP\nAM=M+1\nA=A-1\nM=D\n@SP\nM=M+1\n";
     }
     //static
     if (segment == "static"){
-        temp += "\nD+M\n@16\nA=D+A\nD=M\n@SP\nA=M\nM=D\n";
-    }
-    //local
-    if (segment == "local"){
-        temp += "\nD=A\n@LOCAL\nA=D+M\nD=M\n@SP\nA=M\nM=D\n";
-    }
-    //argument
-    if (segment == "argument"){
-        temp += "\nD=M\n@ARG\nA=D+M\nD=M\n@SP\nA=M\nM=D\n";
+        temp += "\nD+M\n@16\nA=D+A\nD=M\n@SP\nA=M\nM=D\nM=M+1\n";
     }
     //pointer
     if (segment == "pointer"){
-        temp += "\nD+M\n@3\nA=D+A\nD=M\n@SP\nA=M\nM=D\n";
+        temp += "\nD+M\n@3\nA=D+A\nD=M\n@SP\nA=M\nM=D\nM=M+1\n";
+    }
+    //temp
+    if (segment == "temp"){
+        temp += "\nD+M\n@5\nA=D+A\nD=M\n@SP\nA=M\nM=D\nM=M+1\n";
+    }
+    //local
+    if (segment == "local"){
+        temp += "\nD=A\n@LOCAL\nA=D+M\nD=M\n@SP\nA=M\nM=D\nM=M+1\n";
+    }
+    //argument
+    if (segment == "argument"){
+        temp += "\nD=M\n@ARG\nA=D+M\nD=M\n@SP\nA=M\nM=D\nM=M+1\n";
     }
     //this
     if (segment == "this"){
-        temp += "\nD+M\n@THIS\nA=D+A\nD=M\n@SP\nA=M\nM=D\n";
+        temp += "\nD+M\n@THIS\nA=D+A\nD=M\n@SP\nA=M\nM=D\nM=M+1\n";
     }
     //that
     if (segment == "that"){
-        temp += "\nD+M\n@THAT\nA=D+A\nD=M\n@SP\nA=M\nM=D\n";
+        temp += "\nD+M\n@THAT\nA=D+A\nD=M\n@SP\nA=M\nM=D\nM=M+1\n";
     }    
-    //temp
-    if (segment == "temp"){
-        temp += "\nD+M\n@5\nA=D+A\nD=M\n@SP\nA=M\nM=D\n";
-    }
     return temp;;
 }
 
@@ -69,7 +69,7 @@ string VMTranslator::vm_add(){
 
 /** Generate Hack Assembly code for a VM sub operation */
 string VMTranslator::vm_sub(){
-    return "@SP\nAM=M-1\nD=M\nA=A-1\nM=D-M\n";
+    return "@SP\nAM=M-1\nD=M\nA=A-1\nM=M-D\n";
 }
 
 /** Generate Hack Assembly code for a VM neg operation */
